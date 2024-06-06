7808aaf734eafaae6a7d67b409822fc95b17de7c
unknown <mac46099@gmail.com>
2024-06-06 16:34:57 +0800

    bun project initial commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+// Typescript can run in bun
+const num = 16;
+
+const read = (text: string) => {
+	return text;
+};
+
+const message = read('Hello world!');
+console.log(message); // "bun run index.ts" - on terminal to run
